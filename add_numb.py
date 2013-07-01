816c50d26d7a6db3d4da8d90fce655d3fc180f32
K Dhakal <kundan@okstate.edu>
2013-07-01 15:28:20 -0400

    Added modeule with functions

--- add_numb.py
+++ add_numb.py
@@ -1,11 +1,12 @@
+
 #Short comment
 
-def add_two(a,b):
-   return a+b
+from adding import add_two, add_three
+
 
 def main():
-   print ("2 + 3 = ",add_two(2,3))
-
+   print ("2 +2 + 2 = ",add_two(2,2,2))
+   print ("2+3=", add_two(2,3))
 if __name__ == "__main__":
      main()
 
